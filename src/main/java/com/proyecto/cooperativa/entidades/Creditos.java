b2dbc7d62f33fabb3062e052ed05055201d43bd6
angeljohan1 <angeljohan1@gmail.com>
2022-08-30 19:40:53 -0500

    Finalizacion de creacion de entidades y relaciones entre tablas

--- src/main/java/com/proyecto/cooperativa/entidades/Creditos.java
+++ src/main/java/com/proyecto/cooperativa/entidades/Creditos.java
@@ -1,4 +1,85 @@
 package com.proyecto.cooperativa.entidades;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
 public class Creditos {
+    @Id
+    private int codigo;
+
+    @Column(name = "monto-prestado", nullable = false)
+    private int montoPrestado;
+
+    @Column(name = "plazo-prestado", nullable = false)
+    private int plazoPrestado;
+
+    @Column(name = "fecha-desembolso", nullable = false)
+    private Date fechaDesembolso;
+
+    @ManyToOne
+    @JoinColumn(name = "documento-cliente")
+    Cliente cli;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo-lineas-credito")
+    LineasDeCredito lc;
+
+    public Creditos(int codigo, int montoPrestado, int plazoPrestado, Date fechaDesembolso, Cliente cli, LineasDeCredito lc) {
+        this.codigo = codigo;
+        this.montoPrestado = montoPrestado;
+        this.plazoPrestado = plazoPrestado;
+        this.fechaDesembolso = fechaDesembolso;
+        this.cli = cli;
+        this.lc = lc;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getMontoPrestado() {
+        return montoPrestado;
+    }
+
+    public void setMontoPrestado(int montoPrestado) {
+        this.montoPrestado = montoPrestado;
+    }
+
+    public int getPlazoPrestado() {
+        return plazoPrestado;
+    }
+
+    public void setPlazoPrestado(int plazoPrestado) {
+        this.plazoPrestado = plazoPrestado;
+    }
+
+    public Date getFechaDesembolso() {
+        return fechaDesembolso;
+    }
+
+    public void setFechaDesembolso(Date fechaDesembolso) {
+        this.fechaDesembolso = fechaDesembolso;
+    }
+
+    public Cliente getCli() {
+        return cli;
+    }
+
+    public void setCli(Cliente cli) {
+        this.cli = cli;
+    }
+
+    public LineasDeCredito getLc() {
+        return lc;
+    }
+
+    public void setLc(LineasDeCredito lc) {
+        this.lc = lc;
+    }
 }
